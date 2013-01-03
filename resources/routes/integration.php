308e618afdede1d056517b54857eab1e87cf492f
Leandro <lleitep3@gmail.com>
2013-01-02 22:31:06 -0200

    add error log

--- resources/routes/integration.php
+++ resources/routes/integration.php
@@ -3,31 +3,31 @@
 use Site\Service\Integration\Linkedin;
 
 $router->get('/linkedin', function() {
-        $apiKey = 'vj3oxvlgfpni';
-        $apiSecret = 'zVpY54LtLH0cTuYr';
-        $userToken = 'ca62e630-5165-4a47-b93d-002bad5f1394';
-        $userSecret = '261e79e0-c301-42e5-a806-ed8e64f4a099';
-        $resources = array(
-            'email-address'
-            , 'skills'
-            , 'publications'
-            , 'three-current-positions'
-            , 'three-past-positions'
-            , 'specialties'
-            , 'interests'
-            , 'certifications'
-            , 'educations'
-            , 'recommendations-received'
-        );
-        try{
-            $linkedin = new Linkedin($apiKey, $apiSecret);
-            $linkedin->setUserToken($userToken, $userSecret);
-            $linkedin->setResources($resources);
-            $json = $linkedin->get();
-            file_put_contents(PATH_CACHE .DIRECTORY_SEPARATOR. 'linkedIn.json', $json);
-            echo $json;
-        }  catch (\Exception $e){
-            echo file_get_contents(PATH_CACHE .DIRECTORY_SEPARATOR. 'linkedIn.json');
-        }
-        
-    });
+            $apiKey = 'vj3oxvlgfpni';
+            $apiSecret = 'zVpY54LtLH0cTuYr';
+            $userToken = 'ca62e630-5165-4a47-b93d-002bad5f1394';
+            $userSecret = '261e79e0-c301-42e5-a806-ed8e64f4a099';
+            $resources = array(
+                'email-address'
+                , 'skills'
+                , 'publications'
+                , 'three-current-positions'
+                , 'three-past-positions'
+                , 'specialties'
+                , 'interests'
+                , 'certifications'
+                , 'educations'
+                , 'recommendations-received'
+            );
+            try {
+                $linkedin = new Linkedin($apiKey, $apiSecret);
+                $linkedin->setUserToken($userToken, $userSecret);
+                $linkedin->setResources($resources);
+                $json = $linkedin->get();
+                file_put_contents(PATH_CACHE . DIRECTORY_SEPARATOR . 'linkedIn.json', $json);
+                echo $json;
+            } catch (\Exception $e) {
+                error_log('erro ao consultar linkedIn :' . $e->getMessage());
+                echo file_get_contents(PATH_CACHE . DIRECTORY_SEPARATOR . 'linkedIn.json');
+            }
+        });
