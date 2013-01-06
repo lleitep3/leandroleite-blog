562f72def740acbe528a80394a40586be3f50aa2
Leandro <lleitep3@gmail.com>
2013-01-06 13:35:05 -0200

    added new routes by articles

--- resources/routes/integration.php
+++ resources/routes/integration.php
@@ -1,6 +1,8 @@
 <?php
 
 use Site\Service\Integration\Linkedin;
+use Site\Service\Integration\GitHubPublicClient;
+use Site\Service\CurlService;
 
 $router->get('/linkedin', function() {
             $file = PATH_CACHE . DIRECTORY_SEPARATOR . 'linkedIn.json';
@@ -40,17 +42,15 @@ $router->get('/linkedin', function() {
         });
 
 $router->get('/githubrepos', function() {
-            $url = 'https://api.github.com/users/lleitep3/repos?type=owner&sort=pushed';
+            $gitHubClient = new GitHubPublicClient(new CurlService(), 'lleitep3');
             $file = PATH_CACHE . DIRECTORY_SEPARATOR . 'gitHubRepos.json';
-            $curl = new Site\Service\CurlService();
-            $json = $curl->get($url)->fetch();
+            if (!file_exists($file)) {
+                touch($file);
+                chmod($file, 0666);
+            }
 
             try {
-                if (!file_exists($file)) {
-                    touch($file);
-                }
-                chmod($file, 0666);
-
+                $json = $gitHubClient->listRepos(array('type' => 'owner', 'sort' => 'pushed'));
                 file_put_contents($file, $json);
                 $array = json_decode($json);
                 echo json_encode(
@@ -62,6 +62,42 @@ $router->get('/githubrepos', function() {
                 );
             } catch (Exception $e) {
                 error_log('erro ao consultar githubRepos :' . $e->getMessage());
+                echo file_get_contents($file);
+            }
+        });
+
+$router->get('/githubrepo/articles', function() {
+            $gitHubClient = new GitHubPublicClient(new CurlService(), 'lleitep3');
+            $file = PATH_CACHE . DIRECTORY_SEPARATOR . 'gitHubRepo_Articles.json';
+            if (!file_exists($file)) {
+                touch($file);
+                chmod($file, 0666);
+            }
+
+            try {
+                $json = $gitHubClient->getRepoContents('Artigos');
+                file_put_contents($file, $json);
+                echo $json;
+            } catch (Exception $e) {
+                error_log('erro ao consultar githubRepo/articles :' . $e->getMessage());
+                echo file_get_contents($file);
+            }
+        });
+
+$router->get('/githubrepo/article/*', function($articleName) {
+            $gitHubClient = new GitHubPublicClient(new CurlService(), 'lleitep3');
+            $file = PATH_CACHE . DIRECTORY_SEPARATOR . 'gitHubRepo_Articles.json';
+            if (!file_exists($file)) {
+                touch($file);
+                chmod($file, 0666);
+            }
+
+            try {
+                $json = $gitHubClient->getRepoContent('Artigos', $articleName);
+                file_put_contents($file, $json);
+                echo $json;
+            } catch (Exception $e) {
+                error_log('erro ao consultar githubRepo/articles :' . $e->getMessage());
                 echo file_get_contents($file);
             }
         });
