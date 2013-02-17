fd1d9efa82e57fe5e84420a3086c94e69e3de4d0
Leandro <lleitep3@gmail.com>
2013-02-17 13:15:01 -0300

    one more test

---
+++ Vendor/GoogleAPIClient/GoogleClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GoogleAPIClient;
+
+require_once realpath(__DIR__ . '/google-api-php-client/src/Google_Client.php');
+
+/**
+ * Description of GoogleClient
+ *
+ * @author leandro
+ */
+class GoogleClient extends \Google_Client {
+    
+}
